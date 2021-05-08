b66ad7f46450cc8c8396b1e1879a96474612d017
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-07 23:23:53 -0300

    exercicios repetição

--- java/AULAS/src/listajavaExerciciosrepeticao/exercicio01.java
+++ java/AULAS/src/listajavaExerciciosrepeticao/exercicio01.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class exercicio01 {
 	public static void main(String args[]) {
 	
+		//Informar todos os números de 1000 a 1999 
+		//que quando divididos por 11 obtemos resto = 5. (FOR)
 		
 		Scanner leia = new Scanner (System.in);
 		int i;
