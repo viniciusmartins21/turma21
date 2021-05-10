6b8b284ebc641febd97a9a931658221954686421
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-10 17:08:29 -0300

    exercicio atributos aviao

---
+++ java/AULAS/src/lista05aviao/Aviao.java
@@ -0,0 +1,33 @@
+package lista05aviao;
+
+public class Aviao {
+	
+	// Crie uma classe avião e apresente os atributos e métodos referentes esta classe,
+	// em seguida crie um objeto avião, defina as instancias deste objeto e apresente as informações
+	// deste objeto no console.
+	
+	public String modelo;
+	public String color;
+	public double velocMax;
+	public int capcidadePassageiros;
+
+	public Aviao (String modelo,String color,double velocMax, int capcidadePassageiros)
+	{
+		this.modelo = modelo;
+		this.color = color;
+		this.velocMax=velocMax;
+		this.capcidadePassageiros=capcidadePassageiros;
+	}
+	
+	//pegar todos os dados
+	public String getdadosCompletos() 
+	{
+		String dadosCompletos = "modelo do aviao: "+modelo+"cor:"+color+""
+		+"velocidade maxima ktchau:"+velocMax+" KM/h\n"+"capacidade passageiros:"+capcidadePassageiros;
+		return dadosCompletos;
+		
+	}
+		
+}
+
+	
---
+++ java/AULAS/src/lista05aviao/apresentacaoAviao.java
@@ -0,0 +1,12 @@
+package lista05aviao;
+
+public class apresentacaoAviao {
+
+	public static void main(String[] args)
+	{
+		Aviao aviao1 = new Aviao("boing\n","blue\n", 665.00, 200);
+		System.out.print
+		(aviao1.getdadosCompletos());
+		
+	}
+}
