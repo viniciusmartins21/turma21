f145284941be514f6b0bc6724e2b442d184273f3
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-12 14:43:11 -0300

    lista prog. sequencial Java

---
+++ java/AULAS/src/ProgramaçãoSequencialJava/exercicio02.java
@@ -0,0 +1,10 @@
+package ProgramaçãoSequencialJava;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/AULAS/src/ProgramaçãoSequencialJava/exercicio03.java
@@ -0,0 +1,10 @@
+package ProgramaçãoSequencialJava;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
