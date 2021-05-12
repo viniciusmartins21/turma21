6d9bc88ceeff3aecabbfa22160e8c913f147a78b
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-12 13:39:34 -0300

    exercicios laço repetição java

---
+++ java/AULAS/src/exercicioLaçocRepetiçao/exercicio03.java
@@ -0,0 +1,10 @@
+package exercicioLaçocRepetiçao;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/AULAS/src/exercicioLaçocRepetiçao/exercicio05.java
@@ -0,0 +1,21 @@
+package exercicioLaçocRepetiçao;
+
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio05 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+		
+		int num, soma = 0 ;
+		do {
+			System.out.println("Digite um número inteiro : ");
+			num= leia.nextInt();
+			soma+=num;
+		}while(num!=0);
+		leia.close();
+		System.out.printf("A soma dos valores adicionados é : %d",soma);
+	}
+}
