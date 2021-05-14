d31cd451eb3b37e27db2ba1803fe050334e4776c
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-14 10:16:23 -0300

    Projeto Saude

---
+++ java/ProjetoSaude/src/entidades/Hospital.java
@@ -0,0 +1,115 @@
+package entidades;
+
+public class Hospital {
+	
+	private String nome;
+	private String tipo;
+	private int capacidadePacientes;
+	private String tipoAlas;
+	private int qtdLeitos;
+	private int qtdFuncionarios;
+	private int qtdPacientes;
+	
+	
+	public Hospital() {
+	}
+
+
+	
+	public Hospital(String nome, String tipo, int capacidadePacientes, String tipoAlas, int qtdLeitos,
+			int qtdFuncionarios, int qtdPacientes) {
+		super();
+		this.nome = nome;
+		this.tipo = tipo;
+		this.capacidadePacientes = capacidadePacientes;
+		this.tipoAlas = tipoAlas;
+		this.qtdLeitos = qtdLeitos;
+		this.qtdFuncionarios = qtdFuncionarios;
+		this.qtdPacientes = qtdPacientes;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public int getCapacidadePacientes() {
+		return capacidadePacientes;
+	}
+
+
+	public void setCapacidadePacientes(int capacidadePacientes) {
+		this.capacidadePacientes = capacidadePacientes;
+	}
+
+
+	public String getTipoAlas() {
+		return tipoAlas;
+	}
+
+
+	public void setTipoAlas(String tipoAlas) {
+		this.tipoAlas = tipoAlas;
+	}
+
+
+	public int getQtdLeitos() {
+		return qtdLeitos;
+	}
+
+
+	public void setQtdLeitos(int qtdLeitos) {
+		this.qtdLeitos = qtdLeitos;
+	}
+
+
+	public int getQtdFuncionarios() {
+		return qtdFuncionarios;
+	}
+
+
+	public void setQtdFuncionarios(int qtdFuncionarios) {
+		this.qtdFuncionarios = qtdFuncionarios;
+	}
+	
+	
+	
+	public int getQtdPacientes() {
+		return qtdPacientes;
+	}
+
+
+
+	public void setQtdPacientes(int qtdPacientes) {
+		this.qtdPacientes = qtdPacientes;
+	}
+
+
+
+	public void leitosDisponiveis() {
+		if(qtdLeitos>qtdPacientes) {
+			System.out.println("Leitos disponíveis: "+(qtdLeitos-qtdPacientes));
+		}
+		else {
+			System.out.println("Leitos indisponíveis no momento... ");
+		}
+	}
+
+}
+
---
+++ java/ProjetoSaude/src/programa/Programa.java
@@ -0,0 +1,56 @@
+package programa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entidades.AuxiliarLimpeza;
+import entidades.Enfermeire;
+import entidades.Funcionario;
+import entidades.Hospital;
+import entidades.Medice;
+import entidades.Paciente;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		List<Paciente> listaPaciente = new ArrayList<>();
+		List<Funcionario> listaFunc = new ArrayList<>();
+		
+		listaPaciente.add(new Paciente("Vinicius",21,"468486","O+",false));
+		listaPaciente.add( new Paciente("Dafhne",19,"656565","A+",true));
+		
+		listaFunc.add(new Enfermeire("Bruno",29,"564154","Enfermeiro","Cuidados gerais",52,100.10,40,"64445",205.90));
+		listaFunc.add(new Medice("Malu",20,"5666552","Médica","Pediátra",53,120.10,40,"25885",205.90));
+		listaFunc.add(new AuxiliarLimpeza("Jessica",25,"546226632","Auxiliar de limpeza","Serviços gerais",22,200.00,40,"sala cirurgia"));
+		
+		
+		Hospital hospital = new Hospital("Santa Casa","Público",1000,"5 alas",800,1500,500);
+		System.out.println("Nome hospital: "+hospital.getNome());
+		System.out.println("Tipo hospital: "+hospital.getTipo());
+		System.out.println("Total leitos do hospital: "+hospital.getQtdLeitos());
+		System.out.println("Total leitos ocupados do hospital: "+hospital.getQtdPacientes());
+		hospital.leitosDisponiveis();
+		System.out.println();
+		
+		for(Paciente paciente : listaPaciente) {
+			System.out.println("Nome paciente: " + paciente.getNome());
+			System.out.println("Idade paciente: " + paciente.getIdade());
+			System.out.println("CPF paciente: " + paciente.getCpf());
+			System.out.println("Tipo sanguíneo paciente: " + paciente.getTipoSanguineo());
+			System.out.println();
+			
+		}
+		
+		for(Funcionario func : listaFunc) {
+			System.out.println("Nome funcionário: " + func.getNome());
+			System.out.println("Idade funcionário: " + func.getIdade());
+			System.out.println("CPF funcionário: " + func.getCpf());
+			System.out.println("Profissão funcionário: " + func.getProfissao());
+			System.out.println("Setor: " + func.getSetor());
+			System.out.println("Salario: " + func.calcularSalario());
+			System.out.println();
+			
+		}
+	}
+}
